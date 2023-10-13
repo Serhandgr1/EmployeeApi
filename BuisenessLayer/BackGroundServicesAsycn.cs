e26cb717d75b383cc96b249f61d858a3f153cdff
Serhandgr1 <dogruserhanayberk@gmail.com>
2023-10-13 11:45:28 +0300

    bgServiceBagFix

--- BuisenessLayer/BackGroundServicesAsycn.cs
+++ BuisenessLayer/BackGroundServicesAsycn.cs
@@ -18,20 +18,21 @@ namespace BuisenessLayer
         private readonly IIsContunieMailService _ısContunieMailService;
         private readonly IConfiguration _configuration;
         private readonly IBuiseness _codes;
-        public BackGroundServicesAsycn(IBackGroundService<BgMailModel> backGroundService , IBuiseness codes , IIsContunieMailService ısContunieMailService , IConfiguration configuration)
+        public BackGroundServicesAsycn(IBackGroundService<BgMailModel> backGroundService, IBuiseness codes, IIsContunieMailService ısContunieMailService, IConfiguration configuration)
         {
             _configuration = configuration;
             _backGroundService = backGroundService;
             _ısContunieMailService = ısContunieMailService;
-            _codes = codes; 
+            _codes = codes;
         }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-                var model = await _ısContunieMailService.IsContinueRead(stoppingToken);
-                var db = new DataContext(_configuration);
+            var model = await _ısContunieMailService.IsContinueRead(stoppingToken);
+            var db = new DataContext(_configuration);
             BackGroundServiceControllerModel backGroundServiceController = new BackGroundServiceControllerModel();
-            BackGroundServiceControllerModel newData = new BackGroundServiceControllerModel() ;
-            if (model.Id>=1) 
+            BackGroundServiceControllerModel newData = new BackGroundServiceControllerModel();
+            bool count = true;
+            if (model.Id >= 1)
             {
                 backGroundServiceController = db.BackGroundServiceController.Where(x => x.Id == model.Id).First();
                 backGroundServiceController.Id = model.Id;
@@ -49,42 +50,44 @@ namespace BuisenessLayer
                 newData = backGroundServiceController;
             }
 
-                db.BackGroundServiceController.Attach(backGroundServiceController);
-                EntityEntry entry = db.Entry(backGroundServiceController);
-                foreach (var data in new string[] { "Id", "IsWork", "JobName", "JobArgs", "TryCount", "CreationTime", "NextTryTime", "LastTryTime", "IsAbandoned", "Priority", "ExtraProperties", "ConcurrencyStamp" })
+            db.BackGroundServiceController.Attach(backGroundServiceController);
+            EntityEntry entry = db.Entry(backGroundServiceController);
+            foreach (var data in new string[] { "Id", "IsWork", "JobName", "JobArgs", "TryCount", "CreationTime", "NextTryTime", "LastTryTime", "IsAbandoned", "Priority", "ExtraProperties", "ConcurrencyStamp" })
+            {
+                string change = entry.CurrentValues[data].ToString();
+                string change1 = entry.OriginalValues[data].ToString();
+                //  var work1= Convert.ToBoolean(change1);
+                //  var work = Convert.ToBoolean(change);
+                bool a = change1 == change ? true : false;
+                if (!a)
                 {
-                    string change = entry.CurrentValues[data].ToString();
-                    string change1 = entry.OriginalValues[data].ToString();
-                    //  var work1= Convert.ToBoolean(change1);
-                    //  var work = Convert.ToBoolean(change);
-                    bool a = change1 == change?true:false;
-                    bool count = false;
-                    if (!a)
-                    {
-                    switch (backGroundServiceController.Id) 
-                    {
-                        case 1:     // Id'si 1 olan jop güncellenecek 
-                                 //   db.BackGroundServiceController.Update(newData);
-                                   // db.SaveChanges();
-                                   //    await _ısContunieMailService.IsWorkingAdd(work);
-                            break;
-                        case 2: // Id'si 2 olan jop güncellenecek 
-                               //  db.BackGroundServiceController.Update(newData);
-                            // db.SaveChanges();
-                            //    await _ısContunieMailService.IsWorkingAdd(work);
-                            break;
-                    }
+
+                        switch (backGroundServiceController.Id)
+                        {
+                            case 1:     // Id'si 1 olan jop güncellenecek 
+                                        //   db.BackGroundServiceController.Update(newData);
+                                        // db.SaveChanges();
+                                        //    await _ısContunieMailService.IsWorkingAdd(work);
+
+                                break;
+                            case 2: // Id'si 2 olan jop güncellenecek 
+                                    //  db.BackGroundServiceController.Update(newData);
+                                    // db.SaveChanges();
+                                    //    await _ısContunieMailService.IsWorkingAdd(work);
+                             break;
+                        }
                     //BackGroundServiceControllerModel model1 = new BackGroundServiceControllerModel();
                     //model1.IsWork=work;
                     //  db.BackGroundServiceController.Update(newData);
                     // db.SaveChanges();
                     //    await _ısContunieMailService.IsWorkingAdd(work);
                     // await _ısContunieMailService.IsContinueAdd(model1);
-                    }
+                    break;
+                }
                 // var mail = await _backGroundService.SenMail(stoppingToken);
                 // await _codes.BgMailService(mail);
             }
-                ExecuteAsync(stoppingToken);
+            ExecuteAsync(stoppingToken);
         }
     }
 }
